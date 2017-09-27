ad53129aee538ada56a781f26e623db6d7ece664
lekhanhduy <lekhanhduyduy1201@gmail.com>
2017-09-27 23:48:07 +0700

    create tech blog

--- app/controllers/blog.js
+++ app/controllers/blog.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res)=> {
-    res.json({
-        message: 'this is blog page'
-    })
+    res.render('blog/index')
 })
 
 module.exports = router;
