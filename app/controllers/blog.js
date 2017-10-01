a0a1b4a7e728257b0b9a0804adc15bd6832e1294
lekhanhduy <lekhanhduyduy1201@gmail.com>
2017-10-01 10:41:48 +0700

    post detail

--- app/controllers/blog.js
+++ app/controllers/blog.js
@@ -20,4 +20,23 @@ router.get('/', (req, res)=> {
     })
 })
 
+router.get('/post/:id', (req, res)=> {
+    let data = post_md.getPostById(req.params.id);
+
+    data.then(posts =>{
+        let post = posts[0]
+        let result = {
+            post: post,
+            error: false
+        }
+    res.render('blog/post', {data: result})
+    
+    }).catch(error=>{
+        let result = {
+            error: 'could not get post data'
+        }
+        res.render('blog/index', {data: result})
+        
+    })
+})
 module.exports = router;
